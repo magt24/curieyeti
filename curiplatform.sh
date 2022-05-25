4d555af0983e8bb1fcb50cc5a2d27473fd3c3f7a
magt24 <85585015+magt24@users.noreply.github.com>
2022-05-25 01:16:04 +0100

    Update curiplatform.sh

--- curiplatform.sh
+++ curiplatform.sh
@@ -10,7 +10,7 @@ sudo apt install software-properties-common apt-transport-https -y
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update
 sudo apt upgrade
-sudo apt install php-7.4 libapache2-mod-php7.4 -y
+sudo apt install php7.4 libapache2-mod-php7.4 -y
 sudo systemctl restart apache2
 echo Instalando repositorio de curieplaform
 cd /var/www/html/
